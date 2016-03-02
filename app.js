1fbfaf4cdc6cb73561f8ebbf3095136d9b3f94cc
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-03-01 21:45:01 -0800

    set up very simple sass node app

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-var express = require( 'express' );
-var app = express();
+var express = require( 'express' ),
+    app = express(),
+    path = require( 'path' );
+// ROUTE PATHS
+var routes = require( './routes/index' );
 
-app.get( '/', function ( req, res ) {
-  res.send( 'Hello World!' );
+// View engines setup
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'jade' );
+
+// Static
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+// ROUTES
+app.use( '/', routes );
+
+// catch 404 and forward to error handler
+app.use(function( req, res, next ) {
+  var err = new Error( 'Not Found' );
+  err.status = 404;
+  next( err );
 });
 
-app.listen( 3000, function() {
-  console.log( 'Example app is running on port 3000' );
+// error with no stacktrace sent to user
+app.use(function( err, req, res, next ) {
+  res.status( err.status || 500 );
+  res.render( 'error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+app.listen( 3000 );
+
+console.log( 'The server is running at http://localhost:3000' )
